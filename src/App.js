a394aa9985eb80ff72a305273d5bf2a076e23f2e
Daniel Tulpa <dtulpa16@yahoo.com>
2023-03-01 14:38:56 -0600

    Added Icon To Navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
+import ProjectPage from "./pages/ProjectPage";
 function App() {
   return (
-    <div className="bg-green-500 h-screen">
-      <div class="w-3/4">
+    <div className="bg-green-500 h-screen text-white">
+      <div class="w-3/5 m-auto">
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/my-projects" element={<ProjectPage />} />
         </Routes>
       </div>
     </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,23 @@
 import React from "react";
+import DIcon from "../assets/d-icon.png";
 
 export default function Navbar() {
   return (
     <div class="">
-      <ul class="list-none flex gap-4 align-middle justify-end pt-6">
-        <li>About</li>
-        <li>Projects</li>
+      <ul class="list-none flex gap-3 justify-between align-middle pt-6">
+        <div class="align-middle">
+          <li>
+            <img class="rounded-lg" width="42" src={DIcon} />
+          </li>
+        </div>
+        <div class="list-none flex gap-3 align-middle justify-end ">
+          <li class=" hover:text-blue-900 font-bold self-center text-xl cursor-pointer transition-all duration-150">
+            About
+          </li>
+          <li class=" hover:text-blue-900 font-bold self-center text-xl cursor-pointer transition-all  duration-150">
+            Projects
+          </li>
+        </div>
       </ul>
     </div>
   );
