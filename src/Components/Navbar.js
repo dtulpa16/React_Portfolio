410bad53470ac969c4515937c1ec1ee198de78ec
Daniel Tulpa <dtulpa16@yahoo.com>
2023-03-01 13:38:57 -0600

    Added Components + Boilerplate Pages

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div class="">
+      <ul class="list-none flex gap-4 align-middle justify-end pt-6">
+        <li>About</li>
+        <li>Projects</li>
+      </ul>
+    </div>
+  );
+}
