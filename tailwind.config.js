1cf93b1f9affac7636def3ba7933cda95e56ce86
Daniel Tulpa <dtulpa16@yahoo.com>
2023-03-08 17:37:47 -0600

    Updated Theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,6 @@ module.exports = {
         nunito: ["nunito", "sans-serif"],
         MyFont: ['"My Font"', "serif"], // Ensure fonts with spaces have " " surrounding it.
       },
-      tooltip: {
-        base: "bg-white text-gray-900 text-xs border border-gray-200 rounded-md py-2 px-3",
-      },
     },
   },
   plugins: [],
