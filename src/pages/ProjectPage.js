43d7bebbf0944a74af6b1309b5332e11095f500d
Daniel Tulpa <dtulpa16@yahoo.com>
2023-03-08 19:04:59 -0600

    Removed Border From Project Card

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -41,8 +41,8 @@ export default function ProjectPage() {
       <h1 class="text-center mt-4 text-4xl font-bold">Projects</h1>
       <div class="flex flex-wrap justify-center text-center gap-x-6 items-center gap-y-10 mt-14">
       
-        <div class=" h-250 min-h-[266px] transition-all duration-10 p-3 text-black w-72 shadow-md shadow-gray-500 drop-shadow-sm  rounded-lg gap-4 bg-white flex flex-col justify-center items-center mt-10 border-[1.5px] border-black">
-          <img alt="" src={Jaunt} width="120" class="rounded-full -mt-16  shadow-smdrop-shadow- p-1 bg-white" />
+        <div class=" h-250 min-h-[266px] transition-all duration-10 p-3 text-black w-72 shadow-md shadow-gray-800 drop-shadow-md  rounded-lg gap-4 bg-white flex flex-col justify-center items-center mt-10   border-black">
+          <img alt="" src={Jaunt} width="120" class="rounded-full -mt-16  shadow-smdrop-shadow- p-1 bg-white shadow-gray-300 drop-shadow-md" />
           <h1 class="text-2xl font-bold">Jaunt</h1>
           <div class="text-sm min-h-[100px] flex items-center justify-center">
             <h2>
@@ -99,8 +99,8 @@ export default function ProjectPage() {
           </div>
         </div>
       
-        <div class="h-250 min-h-[266px] transition-all duration-10 cursor-default  p-3  text-black w-72 shadow-md shadow-slate-800 drop-shadow-sm  rounded-lg gap-4 bg-white flex flex-col justify-center items-center mt-10 border-[1.5px] border-black">
-          <img alt="" src={Flex} width="120" class="rounded-full -mt-16  shadow-smdrop-shadow- p-1 bg-white" />
+        <div class="h-250 min-h-[266px] transition-all duration-10 cursor-default  p-3  text-black w-72 shadow-md shadow-slate-800 drop-shadow-md  rounded-lg gap-4 bg-white flex flex-col justify-center items-center mt-10   border-black">
+          <img alt="" src={Flex} width="120" class="rounded-full -mt-16  shadow-smdrop-shadow- p-1 bg-white shadow-slate-300 drop-shadow-md" />
           <h1 class="text-2xl font-bold">Flex Bot</h1>
           <div class="text-sm min-h-[100px] flex items-center justify-center">
             <h2>
@@ -150,8 +150,8 @@ export default function ProjectPage() {
             </Tippy>
           </div>
         </div>
-        <div class="h-250 min-h-[266px] transition-all duration-10 cursor-default  p-3  text-black w-72 shadow-md shadow-slate-800 drop-shadow-sm  rounded-lg gap-4 bg-white flex flex-col justify-center items-center mt-10 border-[1.5px] border-black">
-          <img alt="" src={QQ} width="120" class="rounded-full -mt-16  shadow-smdrop-shadow- p-1 bg-white" />
+        <div class="h-250 min-h-[266px] transition-all duration-10 cursor-default  p-3  text-black w-72 shadow-md shadow-slate-800 drop-shadow-md rounded-lg gap-4 bg-white flex flex-col justify-center items-center mt-10   border-black">
+          <img alt="" src={QQ} width="120" class="rounded-full -mt-16  shadow-smdrop-shadow- p-1 bg-white shadow-slate-300 drop-shadow-md" />
           <h1 class="text-2xl font-bold">Question Queue Bot</h1>
           <div class="text-sm min-h-[100px] flex items-center justify-center">
             <h2>
@@ -201,8 +201,8 @@ export default function ProjectPage() {
             </Tippy>
           </div>
         </div>
-        <div class="h-250 min-h-[266px] transition-all duration-10 cursor-default  p-3  text-black w-72 shadow-md shadow-slate-800 drop-shadow-sm  rounded-lg gap-4 bg-white flex flex-col justify-center items-center mt-10 border-[1.5px] border-black">
-          <img alt="" src={Task} width="120" class="rounded-full -mt-16  shadow-smdrop-shadow- p-1 bg-white" />
+        <div class="h-250 min-h-[266px] transition-all duration-10 cursor-default  p-3   text-black w-72 shadow-slate-800 drop-shadow-md rounded-lg gap-4 bg-white flex flex-col justify-center items-center mt-10   border-black">
+          <img alt="" src={Task} width="120" class="rounded-full -mt-16  shadow-smdrop-shadow- p-1 bg-white shadow-slate-300 drop-shadow-md" />
           <h1 class="text-2xl font-bold">Task Manager</h1>
           <div class="text-sm min-h-[100px] flex items-center justify-center">
             <h2>
@@ -215,7 +215,7 @@ export default function ProjectPage() {
             <button onClick={()=>handleOpen(TaskManagerVid)} class="bg-[#08386b] pt-1 pb-1 pl-4 pr-4 text-white rounded-lg transition-all duration-120 hover:scale-105">View Project!</button>
          
           <h1 class="text-xl font-bold">Tools</h1>
-          <div class="flex flex-row gap-1 flex-wrap pr-2 pl-2 align-middle items-center justify-center">
+          <div class="flex flex-row gap-1 flex-wrap pr-2 pl-2 align-middle items-center justify-center ">
           <Tippy content="ExpressJS">
               <a target="_blank" rel="noreferrer" href="https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs">
                 <img alt="ExressJS" src={ExpressIcon} width="42" height="42" class="border-[1px] rounded-lg p-[3px] border-black  transition-all duration-100 hover:scale-105 cursor-pointer bg-white"/>
