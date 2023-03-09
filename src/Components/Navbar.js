35eed8ac122cf7318b407d889ae5f1ff63280aac
Daniel Tulpa <dtulpa16@yahoo.com>
2023-03-09 15:22:46 -0600

    Editted NavBar + Home Page Styles & Changed Headshot Photo

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -15,28 +15,28 @@ export default function Navbar() {
   const navigate = useNavigate();
   return (
     <div>
-      <ul className="list-none flex gap-3 items-center 2xl:justify-between justify-evenly align-middle h-20 w-3/4 mx-auto flex-row flex-wrap min-w-[340px] ">
+      <ul className="list-none flex gap-3 items-center md:justify-between justify-between align-middle h-20 md:h-24 w-3/4 mx-auto flex-row flex-wrap min-w-[340px] ">
         <div className="align-middle">
           <li onClick={() => navigate("/")}>
-            <img alt="D Icon" className="rounded-lg cursor-pointer 2xl:w-[45px] sm:w-[20px]" width="42" src={DIcon} />
+            <img alt="D Icon" className="rounded-lg cursor-pointer md:w-[55px] w-[35px]"  src={DIcon} />
           </li>
         </div>
-        <div className="list-none flex gap-6 align-middle justify-end ">
+        <div className="list-none flex md:gap-6 gap-3 align-middle justify-end ">
           <li
             onClick={() => navigate("/my-projects")}
-            className=" hover:text-[#08386b] font-bold self-center text-md cursor-pointer transition-all  duration-150"
+            className=" hover:text-[#08386b] font-bold self-center md:text-xl text-base cursor-pointer transition-all  duration-150"
           >
             <h1>Projects</h1>
           </li>
           <li
             onClick={() => navigate("/about")}
-            className=" hover:text-[#08386b] font-bold self-center text-md cursor-pointer transition-all duration-150"
+            className=" hover:text-[#08386b] font-bold self-center md:text-xl text-base cursor-pointer transition-all duration-150"
           >
             <h1>About</h1>
           </li>
           <li
             onClick={handleShow}
-            className=" hover:text-[#08386b] font-bold self-center text-md cursor-pointer transition-all duration-150"
+            className=" hover:text-[#08386b] font-bold self-center md:text-xl text-base cursor-pointer transition-all duration-150"
           >
             <h1>Contact</h1>
           </li>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import HeadShot from "../assets/headshot.jpg";
+import HeadShot from "../assets/headshot2.png";
 export default function HomePage() {
   return (
     <div
-      className=" rounded-lg mt-28 flex flex-col w-4/6 m-auto items-center gap-4 justify-center align-middle text-center min-w-[340px]"
+      className="rounded-lg mt-36 md:mt-36 flex flex-col w-4/6 m-auto items-center gap-4 justify-center align-middle text-center min-w-[340px] cursor-default"
     >
     <title>Welcome!</title>
-
-      <img alt="Dan Tulpa Headshot" width="275" className=" border-gray-50 border border-solid rounded-full mb-3 -mt-24" src={HeadShot} />
-      <h1 className="text-3xl mb-2 text-shadow-md text-white shadow-gray-400 font-bold">Hey there! 👋 The name's Dan.</h1>
-      <h1 className="text-2xl mb-2 text-shadow-md text-white shadow-gray-400 font-semibold">I provide end-to-end solutions for any web development project.</h1>
-      <h1 className="text-xl mb-12 text-shadow-md text-white shadow-gray-400 font-semibold">Happy to have you here!</h1>
+      <img alt="Dan Tulpa Headshot" width="320" className="shadow-md shadow-grey-500 drop-shadow-md rounded-full mb-8 -mt-28 md:-mt-24" src={HeadShot} />
+      <div className="flex flex-col md:gap-8 gap-6">
+        <h1 className="md:text-6xl text-3xl mb-2 text-shadow-md text-white shadow-gray-400 font-bold">Hey there! 👋 The name's Dan.</h1>
+        <h1 className="md:text-3xl text-xl mb-2 text-shadow-md text-white shadow-gray-400 font-semibold">I provide end-to-end solutions for any web development project.</h1>
+        <h1 className="md:text-2xl text-lg mb-12 text-shadow-md text-white shadow-gray-400 font-semibold">Happy to have you here!</h1>
+      </div>
     </div>
   );
 }
